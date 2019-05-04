ba060559c43ba40b1cd7287d8aa4a8fc05bdce71
kkcljy <kkcljy@gmail.com>
2019-05-04 22:02:37 +0900

    업데이트

--- js/common.js
+++ js/common.js
@@ -1,17 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('#baseGnb .depth1-li .depth2-ul').css('opacity' , '0');
-    $('#baseGnb .depth1-li').mouseover(function(){
-        $(this).addClass('active');
-        $(this).find('.depth2-ul').stop().animate({
-            opacity: '100'
-        }, 500);
+
+    $('#baseGnb .depth1-li .depth2-ul').css({
+        opacity: 0,
     });
-    $('#baseGnb .depth1-li').mouseleave(function(){
-        $(this).removeClass('active');
+    $('#baseGnb .depth1-li').mouseover(function () {
+        $(this).addClass('active').css('height','auto');
+        $(this).find('.depth2-ul').stop().animate({
+            opacity: '100',
+        }, 500)
+    });
+    $('#baseGnb .depth1-li').mouseleave(function () {
+        $(this).removeClass('active').css('height','42px');
         $(this).find('.depth2-ul').stop().animate({
             opacity: '0'
-        }, 300);
+        }, 300)
     });
 
 
