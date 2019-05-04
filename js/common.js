db4655fffb727d02143ca8dcd11ebac481645d60
kkcljy <kkcljy@gmail.com>
2019-05-04 23:44:28 +0900

    메뉴

--- js/common.js
+++ js/common.js
@@ -24,6 +24,7 @@ $(document).ready(function () {
     });
 
     $(window).scroll(function () {
+
         if ($(this).scrollTop() > 600) {
             $('.btn-top').fadeIn(200);
         } else {
@@ -35,7 +36,45 @@ $(document).ready(function () {
         $('html').animate({
             scrollTop: 0
         }, 600);
-        
+
     });
+
+
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('.base-header').outerHeight();
+
+    $(window).scroll(function (event) {
+        didScroll = true;
+    });
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop(); 
+
+        if (Math.abs(lastScrollTop - st) <= delta) 
+        return;
+        
+        if (st > lastScrollTop && st > navbarHeight) {
+            // Scroll Down 
+            $('.base-header').fadeOut(500);
+        } else {
+            // Scroll Up 
+            if (st + $(window).height() < $(document).height()) {
+                $('.base-header').fadeIn(500);
+            }
+        }
+        lastScrollTop = st;
+    }
+
+
+
+
 
 });
